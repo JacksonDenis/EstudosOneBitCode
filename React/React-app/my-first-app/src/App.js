842bb137e0345edc91a0d6d61d5b0c90b34b3477
Jackson Denis <jackson.denis@live.com>
2022-04-18 11:55:42 -0300

    Primeiro exercicio React

---
+++ React/React-app/my-first-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HelloWorld from './hello';
+import Lorem from './componets/lorem';
+
+function App() {
+  return (
+    <div>
+      <HelloWorld/>
+      <Lorem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/React-app/my-first-app/src/componets/lorem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Lorem () {
+    return <p>jkasdbfjashdfçahsdfkashdfahsdfjajkbdkjfabskdjfkl</p>
+  }
+
+  export default Lorem;
